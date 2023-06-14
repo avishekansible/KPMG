0d1c962501d40fa35fd46209e322700ec0e74172
Das Avishek (BT - Customer Domain) <a546837@inmac1213.(none)>
2023-06-14 12:27:49 +0530

    made few changes

--- problem2.py
+++ problem2.py
@@ -1,12 +1,18 @@
 import requests
 import json
+import base64   
+
+proxies = {
+    "http": None,
+    "https": None
+}
 
 def get_azure_instance_metadata():
     url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
     headers = {"Metadata": "true"}
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, proxies=proxies)
         response.raise_for_status()
         metadata = response.json()
         return metadata
@@ -14,15 +20,17 @@ def get_azure_instance_metadata():
         print("Failed to retrieve Azure instance metadata:", e)
         return None
 
-# Example usage
+#def print_json_obj_to_terminal(json_obj):
+#   print(json.dumps(json_obj, sort_keys=True, indent=4, separators=(',', ': ')))
+
 metadata = get_azure_instance_metadata()
 if metadata:
     # Print the entire metadata
-    print(json.dumps(metadata, indent=4))
+    print(json.dumps(metadata, sort_keys=True, indent=4, separators=(',', ': ')))
 
     # Retrieve a specific data key
     specific_key = "compute"
     if specific_key in metadata:
-        print(json.dumps(metadata[specific_key], indent=4))
+        print(json.dumps(metadata[specific_key], sort_keys=True, indent=4, separators=(',', ': ')))
     else:
         print(f"Key '{specific_key}' not found in the metadata.")
